73f5769f1c817a985605a7386f1cadeb0af359cd
jamesdanielwhitford <70632508+jamesdanielwhitford@users.noreply.github.com>
2024-08-06 11:46:39 +0200

    Port update in main.py

--- app/main.py
+++ app/main.py
@@ -25,4 +25,4 @@ async def read_root(request: Request):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8080)
