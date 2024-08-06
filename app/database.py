6c325c217ee4a09e62516ce7f17b82c1d530ca1d
jamesdanielwhitford <70632508+jamesdanielwhitford@users.noreply.github.com>
2024-08-06 18:43:47 +0200

    update Database host variable

--- app/database.py
+++ app/database.py
@@ -14,7 +14,7 @@ env_path = os.path.join(os.getcwd(), '.env')
 
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_HOST = os.getenv("DB_HOST")
+DB_HOST = os.getenv("EMPLOYEES_HOST")
 DB_NAME = os.getenv("DB_NAME")
 
 
